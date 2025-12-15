0fd8efc53aa41d7fe9ded5793aad392e0b3f34b7
Xing Zhou <xing@xingzhou.me>
2025-12-15 09:05:09 -0500

    feat(day-2-part-2): solved

--- src/aoc_day_2.clj
+++ src/aoc_day_2.clj
@@ -25,6 +25,16 @@
   [id]
   (and (has-even-digits id) (has-twice-repeated-sequence id)))
 
+(defn- is-invalid-part-2
+  ([id]
+   (loop [length 1
+          max-length (quot (count id) 2)
+          s id]
+     (cond (> length max-length) false
+           (is-invalid-part-2 id (subs id 0 length)) true
+           :else (recur (inc length) max-length s))))
+  ([id sub] (empty? (str/split id (re-pattern sub)))))
+
 (defrecord ProductIdRange [start end]
   InvalidIdGenerator
     (generate-invalid-ids [_ is-invalid]
@@ -54,10 +64,15 @@
 (def example
   "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124")
 
-(comment
-  example)
+(let [result (process [example] is-invalid-part-1)]
+  (assert (= 1227775554N result) result))
+
+(let [result (process [example] is-invalid-part-2)]
+  (assert (= 4174379265N result) result))
 
 (with-open [rdr (io/reader (io/resource "input-day-2.txt"))]
   (let [input-lines (line-seq rdr)
-        part-1 (time (process input-lines is-invalid-part-1))]
-    (assert (= 9188031749N part-1) part-1)))
+        part-1 (time (process input-lines is-invalid-part-1))
+        part-2 (time (process input-lines is-invalid-part-2))]
+    (assert (= 9188031749N part-1) part-1)
+    (assert (= 11323661261N part-2) part-2)))
