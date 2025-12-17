c8044a43a2465f67ea4bfba8a04f2b27f76299b0
Xing Zhou <xing@xingzhou.me>
2025-12-17 11:17:07 -0500

    refactor(day-3-part-2): renamed variables to gain clarity

--- src/aoc_day_3.clj
+++ src/aoc_day_3.clj
@@ -1,78 +1,73 @@
 (ns aoc-day-3
   (:require [clojure.java.io :as io]
-            [clojure.set :as set]
-            [clojure.string :as str]))
+            [clojure.set :as set]))
 
 (defn prune
-  [locations selected invalid]
-  (reduce-kv
-   (fn [m digit idxs]
-     (assoc m digit (filter #(not (or (selected %) (invalid %))) idxs)))
-   {}
-   locations))
+  [catalog selected invalid]
+  (let [is-candidate? (comp not (set/union selected invalid))]
+    (reduce-kv #(assoc %1 %2 (filter is-candidate? %3)) {} catalog)))
+
+(defn select-index
+  [idx exclusive-upper-idx remaining selected invalid]
+  (let [bigger (range idx exclusive-upper-idx)
+        smaller (range idx)
+        bigger-contains-remaining?
+        (>= (count (set/difference (set bigger) selected invalid)) remaining)]
+    (if bigger-contains-remaining?
+      [(conj selected idx) (into invalid smaller)]
+      [(into selected bigger) invalid])))
 
 (defn pick-batteries
-  [bank batteries-needed digit-locations]
-  (let [length (count bank)]
+  [bank batteries-needed catalog]
+  (let [exclusive-upper-idx (count bank)]
     (loop [target-digit 9
-           locations digit-locations
+           initial-catalog catalog
            selected #{}
            invalid #{}]
-      (let [pruned-locations (prune locations selected invalid)
+      (let [pruned-catalog (prune initial-catalog selected invalid)
             remaining (- batteries-needed (count selected))
-            [idx] (pruned-locations target-digit)]
-        (cond (zero? remaining) selected ; found the correct number of
-                                         ; batteries
-              (< target-digit 0) (do (println bank) {})
+            [idx] (pruned-catalog target-digit)]
+        (cond (zero? remaining) selected
               (nil? idx) (recur (dec target-digit)
-                                (dissoc pruned-locations target-digit)
+                                (dissoc pruned-catalog target-digit)
                                 selected
-                                invalid) ; no more <target-digit> in the
-                                         ; eligible locations
-              :else (let [selected-with-index (conj selected idx)]
-                      (if (>= (count (set/difference
-                                      (into #{} (range (inc idx) length))
-                                      selected
-                                      invalid))
-                              remaining)
-                        (recur target-digit
-                               pruned-locations
-                               selected-with-index
-                               (into invalid (range idx)))
-                        (recur target-digit
-                               pruned-locations
-                               (into selected-with-index
-                                     (range (inc idx) length))
-                               invalid))))))))
+                                invalid)
+              :else (let [[updated-selected updated-invalid]
+                          (select-index idx
+                                        exclusive-upper-idx
+                                        remaining
+                                        selected
+                                        invalid)]
+                      (recur target-digit
+                             pruned-catalog
+                             updated-selected
+                             updated-invalid)))))))
 
 
-(defn- extract-digit-locations
+(defn- catalog-batteries
   [^String bank]
-  (->> (map-indexed vector bank)
-       (reduce (fn [m [idx d]]
-                 (let [digit (^[char int] Character/digit d 10)]
-                   (->> idx
-                        (conj (m digit []))
-                        (assoc m digit))))
-               {})))
+  (->> (vec bank)
+       (reduce-kv (fn [m idx d]
+                    (let [digit (^[char int] Character/digit d 10)]
+                      (as-> (m digit []) $ (conj $ idx) (assoc m digit $))))
+                  {})))
 
 (defn combine-batteries
   [bank selected-indices]
-  (reduce str
-          (map second
-               (filter #(selected-indices (first %))
-                       (map-indexed vector bank)))))
+  (->> (map-indexed vector bank)
+       (filter #(selected-indices (first %)))
+       (map second)
+       (reduce str)))
 
 (defn find-joltage
   [bank batteries-needed]
-  (->> (extract-digit-locations bank)
+  (->> (catalog-batteries bank)
        (pick-batteries bank batteries-needed)
        (combine-batteries bank)))
 
 (defn process
   [input batteries-needed]
   (->> (map #(find-joltage %1 batteries-needed) input)
-       (map str/join)
        (reduce #(+ %1 (bigint %2)) 0)))
 
 (comment
@@ -80,10 +75,11 @@
     ["2213322222223222132231233322432224423222226232323522232215252332221122222231232224232722131522422232"])
   (bigint "123")
   (process example 12)
-  (let [value (process example 2)] (assert (= 357 value) value))
-  (with-open [rdr (io/reader (io/resource "input-day-3.txt"))]
-    (let [input-lines (line-seq rdr)
-          part-1 (time (process input-lines 2))
-          part-2 (time (process input-lines 12))]
-      (assert (= 17034 part-1) part-1)
-      (assert (= 168798209663590N part-2) part-2))))
+  (let [value (process example 2)] (assert (= 357 value) value)))
+
+(with-open [rdr (io/reader (io/resource "input-day-3.txt"))]
+  (let [input-lines (line-seq rdr)
+        part-1 (time (process input-lines 2))
+        part-2 (time (process input-lines 12))]
+    (assert (= 17034 part-1) part-1)
+    (assert (= 168798209663590N part-2) part-2)))
