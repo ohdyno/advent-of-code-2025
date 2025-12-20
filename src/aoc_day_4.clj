622b98ebf28778d70be3f30815f1cfd2a02b03d4
Xing Zhou <xing@xingzhou.me>
2025-12-20 00:02:01 -0500

    feat(day-4-part-1): initial implementation, but incorrect result

--- src/aoc_day_4.clj
+++ src/aoc_day_4.clj
@@ -1,12 +1,11 @@
 (ns aoc-day-4
   (:require [clojure.java.io :as io]
-            [clojure.spec.alpha :as spec]
             [clojure.math.combinatorics :as combo]))
 
 (defn catalog
   [input-lines]
   (reduce (fn [m {row :row, col :col, is-role :is-roll}]
-            (update m is-role #(if (nil? %1) #{[row, col]} (conj %1 [row, col]))))
+            (update m is-role #(if (nil? %1) #{[row col]} (conj %1 [row col]))))
           {}
           (reduce into
                   (map-indexed (fn [row-number row]
@@ -19,50 +18,43 @@
                                               row))
                                input-lines))))
 
-(defn foo
-  [rows cols rolls neighbors [r c]]
-  (let [neighbors-to-update (filter (fn [[row col :as cell]] (and (not (and (= r row) (= c col)))
-                                                         (spec/int-in-range? 0 rows row)
-                                                         (spec/int-in-range? 0 cols col)
-                                                         (some #{cell} rolls))) (combo/cartesian-product (range (dec r) (+ r 2))
-                                                                                                                        (range (dec c) (+ c 2))))]
-    (reduce (fn [m cell] (update m cell #(if (nil? %1) 1 (inc %1)))) neighbors neighbors-to-update)
-    )
-  )
-
 (defn count-neighbors
-  [rows cols cataloged]
-  (->> (:roll cataloged)
-       (reduce #(foo rows cols (:roll cataloged) %1 %2) {})))
+  [rows cols rolls]
+  (let [rolls-set (set rolls)]
+    (->> rolls
+         (map (fn [[r c]]
+                (->> (combo/cartesian-product (range (dec r) (+ r 2))
+                                              (range (dec c) (+ c 2)))
+                     (filter (fn [[r' c']] (not (and (= r r') (= c c')))))
+                     (filter (fn [[r c]]
+                               (and (<= 0 r (dec rows))
+                                    (<= 0 c (dec cols))))))))
+         (map frequencies)
+         (reduce (partial merge-with +))
+         (filter #(rolls-set (first %))))))
 
-(let [input-lines [".@@"
-                   "@.@"
-                   "@@@"]
+(let [input-lines [".@@" "@.@" "@@@"]
       cataloged (catalog input-lines)
       rows (count input-lines)
       cols (count (first input-lines))]
-  (->> cataloged
-       ))
+  (->> (:roll cataloged)
+       (count-neighbors rows cols)
+       (filter #(< (second %) 4))
+       (count)))
 
 (defn process
   [input-lines]
   (let [cataloged (catalog input-lines)
         rows (count input-lines)
         cols (count (first input-lines))]
-    (->> cataloged
-         )))
+    (->> (:roll cataloged)
+         (count-neighbors rows cols)
+         (filter #(< (second %) 4))
+         (count))))
 
 (def example
-  ["..@@.@@@@."
-  "@@@.@.@.@@"
-  "@@@@@.@.@@"
-  "@.@@@@..@."
-  "@@.@@@@.@@"
-  ".@@@@@@@.@"
-   ".@.@.@.@@@"
-   "@.@@@.@@@@"
-   ".@@@@@@@@."
-   "@.@.@@@.@."])
+  ["..@@.@@@@." "@@@.@.@.@@" "@@@@@.@.@@" "@.@@@@..@." "@@.@@@@.@@" ".@@@@@@@.@"
+   ".@.@.@.@@@" "@.@@@.@@@@" ".@@@@@@@@." "@.@.@@@.@."])
 
 (time (process example))
 
@@ -73,5 +65,4 @@
     (let [input-lines (line-seq rdr)
           part-1 (time (process (vec input-lines)))
           part-2 nil]
-      (assert (= 1512 part-1) part-1)
-      (assert (= nil part-2)))))
+      (assert (= 1512 part-1) part-1))))
