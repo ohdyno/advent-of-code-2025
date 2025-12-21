41e07f0005f58f02e95c16300b04259b190365c7
Xing Zhou <xing@xingzhou.me>
2025-12-21 11:29:36 -0500

    feat(6-2): solved!

--- src/day6/aoc.clj
+++ src/day6/aoc.clj
@@ -16,20 +16,25 @@
          (map second)
          (reduce +))))
 
-(defn extract-numbers [max-lengths input-line]
+(defn extract-numbers
+  [max-lengths input-line]
   (loop [line input-line
          [length & remaining-length] max-lengths
          numbers []]
-    (if (nil? length) numbers
-                      (recur (drop (inc length) line) remaining-length (conj numbers (take length line)))))
-  )
+    (if (nil? length)
+      numbers
+      (recur (drop (inc length) line)
+             remaining-length
+             (conj numbers (take length line))))))
 
-(defn find-max-length-per-column [values]
+(defn find-max-length-per-column
+  [values]
   (->> (map #(str/split (str/trim %) #"\s+") values)
        (map #(map count %))
        (reduce #(map max %1 %2))))
 
-(defn process-2 [input-lines]
+(defn process-2
+  [input-lines]
   (let [ops (->> (str/split (last input-lines) #"\s+")
                  (map #(if (= "*" %) (resolve '*) (resolve '+))))
         values (drop-last input-lines)
@@ -39,21 +44,13 @@
          (map #(apply map vector %))
          (map #(map (comp bigint str/trim str/join) %))
          (map #(apply %1 %2) ops)
-         (reduce +)
-         )))
+         (reduce +))))
 
-(let
-  [input-lines [
-  "123 328  51 64 "
-  " 45 64  387 23 "
-  "  6 98  215 314"
-  "*   +   *   +  "]
-   ]
+(let [input-lines ["123 328  51 64 " " 45 64  387 23 " "  6 98  215 314"
+                   "*   +   *   +  "]]
   (assert (test/is (= 3263827N (process-2 input-lines)))))
 
 (with-open [rdr (io/reader (io/resource "day6/input.txt"))]
   (let [input-lines (line-seq rdr)]
-    (comment (assert (test/is (= 707 (time (process input-lines)))))
-             (assert (test/is (= 361615643045059N nil))))
-    (second input-lines)
-    ))
+    (assert (test/is (= 4412382293768N (time (process input-lines)))))
+    (assert (test/is (= 7858808482092N (time (process-2 input-lines)))))))
