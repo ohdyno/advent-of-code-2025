ca38db00aab36624e61c4b46a6ec5ad2b89263fd
Xing Zhou <xing@xingzhou.me>
2025-12-20 21:28:32 -0500

    feat(day-4-part-2): solved!

--- src/aoc_day_4.clj
+++ src/aoc_day_4.clj
@@ -19,43 +19,53 @@
                  (and (<= 0 r') (< r' rows) (<= 0 c') (< c' cols))))))
 
 (defn count-adjacent
+  "Return a map of roll location and the number of adjacent rolls."
   [rows cols rolls]
   (->> rolls
        (map (fn [[r c :as cell]]
               (let [adjacent-cells (generate-adjacent r c rows cols)]
                 {cell (count (filter rolls adjacent-cells))})))))
 
-(let [input-lines ["@@" "@@"]
+(defn remove-rolls
+  [rows cols cataloged]
+  (let [adjacents (count-adjacent rows cols cataloged)
+        removal-candidates (filter #(< (first (vals %)) 4) adjacents)]
+    {:removed-count (count removal-candidates),
+     :remaining (apply disj cataloged (mapcat keys removal-candidates))}))
+
+(let [input-lines ["@@@" ".@@"]
       cataloged (catalog input-lines)
       rows (count input-lines)
       cols (count (first input-lines))]
-  (->> cataloged
-       (count-adjacent rows cols)
-       (map #(first (vals %)))
-       (filter #(< %1 4))))
+  (remove-rolls rows cols cataloged))
 
 (defn process
-  [input-lines]
-  (let [cataloged (catalog input-lines)
-        rows (count input-lines)
-        cols (count (first input-lines))]
-    (->> cataloged
-         (count-adjacent rows cols)
-         (map #(first (vals %)))
-         (filter #(< %1 4))
-         (count))))
+  ([input-lines opt]
+   (let [cataloged (catalog input-lines)
+         rows (count input-lines)
+         cols (count (first input-lines))]
+     (cond (= opt :once) (:removed-count (remove-rolls rows cols cataloged))
+           :else (loop [total-removed 0
+                        rolls-remaining cataloged]
+                   (let [{removed :removed-count, remaining :remaining}
+                         (remove-rolls rows cols rolls-remaining)]
+                     (if (zero? removed)
+                       total-removed
+                       (recur (+ total-removed removed) remaining))))))))
 
 (def example
   ["..@@.@@@@." "@@@.@.@.@@" "@@@@@.@.@@" "@.@@@@..@." "@@.@@@@.@@" ".@@@@@@@.@"
    ".@.@.@.@@@" "@.@@@.@@@@" ".@@@@@@@@." "@.@.@@@.@."])
 
-(let [result (time (process example))]
+(let [result (time (process example :once))]
   (assert (= 13 result))
   result)
 
-(comment
-  (with-open [rdr (io/reader (io/resource "input-day-4.txt"))]
-    (let [input-lines (line-seq rdr)
-          part-1 (time (process (vec input-lines)))
-          part-2 nil]
-      (assert (= 1516 part-1) part-1))))
+(let [result (time (process example :repeat))] (assert (= 43 result) result))
+
+(with-open [rdr (io/reader (io/resource "input-day-4.txt"))]
+  (let [input-lines (line-seq rdr)
+        part-1 (time (process (vec input-lines) :once))
+        part-2 (time (process (vec input-lines) :repeat))]
+    (assert (= 1516 part-1) part-1)
+    (assert (= 9122 part-2) part-2)))
