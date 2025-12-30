26dc7d2cfa91b1fdbe7d62f8c2481b4c0170a76c
Xing Zhou <xing@xingzhou.me>
2025-12-29 23:49:45 -0500

    docs(8-2): add instructions

--- src/day8/aoc.clj
+++ src/day8/aoc.clj
@@ -57,6 +57,8 @@
        (reduce *)))
 
 ;!zprint {:format :skip}
+(defn process-2 [input-lines])
+
 (let [input-lines
       ["162,817,812"
        "57,618,57"
@@ -81,7 +83,29 @@
       ]
   (assert (test/is (= 40 (process input-lines)))))
 
-(defn process-2 [input-lines])
+(let [input-lines
+      ["162,817,812"
+       "57,618,57"
+       "906,360,560"
+       "592,479,940"
+       "352,342,300"
+       "466,668,158"
+       "542,29,236"
+       "431,825,988"
+       "739,650,466"
+       "52,470,668"
+       "216,146,977"
+       "819,987,18"
+       "117,168,530"
+       "805,96,715"
+       "346,949,466"
+       "970,615,88"
+       "941,993,340"
+       "862,61,35"
+       "984,92,344"
+       "425,690,689"]
+      ]
+  (assert (test/is (= 25272 (process-2 input-lines)))))
 
 (comment
   (with-open [rdr (io/reader (io/resource "day8/input.txt"))]
